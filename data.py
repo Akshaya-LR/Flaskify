46d5dde6cea96d9e2ef38c6ec53d2ec7af562960
Akshaya <akshayasharma014@gmail.com>
2021-11-26 19:25:59 +0530

    git commit -m 'initial commit'

---
+++ data.py
@@ -0,0 +1,32 @@
+def articles():
+    article = [
+        {
+            "id": 1,
+            "name": "List and List Comprehension in Python",
+            "body": "List is one of the data structures in Python. They are used to store multiple items of "
+                    "different data types. That is, a list can have an integer, a floating-point number, "
+                    "a string or all three together in a single list. The items in a list are separated by commas "
+                    "and are enclosed in square brackets [ ]."
+                    "The list items are ordered, mutable and can contain duplicate values. Ordered - Lists have a "
+                    "definite order.Mutable - The list items can be changed ie., it can be removed, "
+                    "added or replaced with a new item. Can contain duplicate values- same item can exist "
+                    "multiple times in a single list.",
+            "date": "Sep 10, 2021",
+            "author": "Akshaya LR",
+            "link": "https://akshayalr.hashnode.dev/list-and-list-comprehension-in-python"
+        },
+        {
+            "id": 2,
+            "name": "Python Decorators",
+            "body": "A decorator is a function that takes another function and adds some functionality to it, "
+                    "without modifying it."
+                    "Before trying to understand decorators, we must know about the functions. Let's talk about "
+                    "the BIG 3 about functions.",
+            "date": "Oct 25, 2021",
+            "author": "Akshaya LR",
+            "link": "https://akshayalr.hashnode.dev/python-decorators"
+        }
+
+    ]
+    return article
+
